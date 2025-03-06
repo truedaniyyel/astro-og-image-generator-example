23e973913a6310e282390dae57e4650d53d051e7
off-origami <baskakovihor@gmail.com>
2025-03-06 03:17:03 -0800

    init

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+import type { Site } from './types';
+
+export const SITE: Site = {
+  TITLE: 'Sharing practical design tips',
+  DESCRIPTION: 'From room layout secrets to color psychology',
+  AUTHOR: 'Lisa Brown',
+};
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type Site = {
+  TITLE: string;
+  DESCRIPTION: string;
+  AUTHOR: string;
+};
